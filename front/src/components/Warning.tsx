b208d7316710e9c8cdb0813e82e9a27821232b86
Acemanu412 <manuel.n.aceituno@gmail.com>
2019-11-28 14:09:11 -0300

    Warning vuelve a ser un string vacío cada vez que se cambia de pantalla

--- front/src/components/Warning.tsx
+++ front/src/components/Warning.tsx
@@ -1,5 +1,5 @@
 import {observer} from "mobx-react";
-import React from "react";
+import React, {useEffect} from "react";
 import { useStores } from "../hooks/useStore";
 
 interface IWarningProps extends React.HTMLAttributes<HTMLDivElement> {
@@ -7,5 +7,10 @@ interface IWarningProps extends React.HTMLAttributes<HTMLDivElement> {
 
 export const Warning = observer((props: IWarningProps) => {
     const store = useStores();
+    useEffect(() => {
+        return () => {
+          store.updateWarning({message: ""});
+        };
+      }, [store]);
     return <div {...props}>{store.warning}</div>;
 });
