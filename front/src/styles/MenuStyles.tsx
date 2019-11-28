bb1714ba3d0d94af66ecff19ca52fc8833e3db0d
Federico <fariganello@gmail.com>
2019-11-28 13:33:02 -0300

    adding home route

---
+++ front/src/styles/MenuStyles.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const LogoMenu = styled.div`
+margin: 2px;
+font-size: 1rem;
+font-weight: bold;
+color:yellow;
+width:100%;
+height: 2rem;
+box-sizing:border-box;
+padding: 7px;
+`;
--- front/src/styles/NavBarStyles.tsx
+++ front/src/styles/NavBarStyles.tsx
@@ -1,7 +1,24 @@
 import styled from "styled-components";
+import { Menu } from "../components/Menu";
+
+export const StyledMenu = styled(Menu)`
+  
+
+
+}
+`;
 
 export const LogoNavBar = styled.img`
-  width: 1.5rem;
-  height: 1.5rem;
-  margin-right: 1rem;
+  position: fixed;
+  right: 4%;
+  top: 1.2%
+  height: 2rem;
+`;
+
+export const NavBarContainer = styled.div`
+position:fixed;
+top: 0;
+width: 100%;
+height: 8%;
+background-color:#0056A2;
 `;
