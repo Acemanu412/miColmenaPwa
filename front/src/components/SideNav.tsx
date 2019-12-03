06afb08e5329b4d1461b4f85e553859470d938ff
jsmil122 <jsmilbank@gmail.com>
2019-12-03 14:17:39 -0300

    alphebetize imports

--- front/src/components/SideNav.tsx
+++ front/src/components/SideNav.tsx
@@ -1,7 +1,8 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { logOut } from "../api/index";
 import { withRouter } from "react-router";
+import { Link } from "react-router-dom";
+
+import { logOut } from "../api/index";
 import { ImgDiv, LinkDiv, LinkImg, LinkTextSpan, SideNavImg, SideNavMenu } from "../styles/SideNavStyles";
 
 const SideNav: React.FC = (props: any) => {
