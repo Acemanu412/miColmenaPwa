f8d83e2a871d040dd6d197695002ec76de594258
Erika Stefania Castillo Barragan <erikaastef99@10.100.0.72>
2019-12-06 13:32:42 -0300

    agregrar colmenas esta mas bonita

--- front/src/styles/AgregarColmenaEstandarStyles.tsx
+++ front/src/styles/AgregarColmenaEstandarStyles.tsx
@@ -6,7 +6,7 @@ import { color } from "../themes";
 const { amarillo } = color;
 
 export const StyledForm = styled.form`
-  padding: 0.5rem;
+  background-color: #f9f9f9;
 `;
 
 export const TextoAmarillo = styled.span`
@@ -17,24 +17,49 @@ export const TextoAmarillo = styled.span`
 `;
 
 export const FormContent = styled.div`
-  margin: 1rem;
+  margin-bottom: 1rem;
+  border-bottom: 1px solid;
+  border-top: 1px solid;
+  border-color: #F2F0F0
+  padding: 2rem;
+  width: 100%;
+  background-color: #ffff
 `;
 
 export const StyledInput = styled(Input)`
   margin-top: 1rem;
+  width: 70vw;
+  height: 5vh;
+  outline: none;
+  border-radius: 2px;
+  border: 1px solid;
+  padding: 0.5rem;
+  border-color: #e5e3e3;
 `;
 
-export const StyledSelect = styled.select``;
+export const StyledSelect = styled.select`
+  width: 70vw;
+  height: 4vh;
+  outline: none;
+  border-radius: 2px;
+  border: 1px solid;
+  padding-left: 0.5rem;
+  border-color: #e5e3e3;
+  background-color: #ffff;
+`;
 
 export const NotaInput = styled.span`
   display: block;
   font-size: 0.7rem;
+  padding-top: 0.5rem;
+  color: #aaa7a7;
 `;
 
 export const ImageContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  
 `;
 
 export const StyledImage = styled.img`
--- front/src/views/AgregarColmenaEstandar.tsx
+++ front/src/views/AgregarColmenaEstandar.tsx
@@ -5,134 +5,192 @@ import { RouteComponentProps } from "react-router-dom";
 import { useForm } from "../hooks/formHook";
 import { useStores } from "../hooks/useStore";
 import {
-    FormContent,
-    ImageContainer,
-    NotaInput,
-    StyledForm,
-    StyledInput,
-    StyledSelect,
-    TextoAmarillo,
+  FormContent,
+  ImageContainer,
+  NotaInput,
+  StyledForm,
+  StyledInput,
+  StyledSelect,
+  TextoAmarillo,
 } from "../styles/AgregarColmenaEstandarStyles";
 import {
-    LogoYellowBanner,
-    SpanBanner,
-    WrapperDivForImages,
-    YellowBanner,
+  LogoYellowBanner,
+  SpanBanner,
+  WrapperDivForImages,
+  YellowBanner,
 } from "../styles/AgregarColmenaStyles";
 import { FormAtrasButton, FormSubmitButton } from "../styles/FormStyles";
 import { NavBar } from "./NavBar";
 
-export const AgregarColmenaEstandar: React.FC<RouteComponentProps> = (props) => {
+export const AgregarColmenaEstandar: React.FC<RouteComponentProps> = (
+  props
+) => {
+  const store = useStores();
 
-    const store = useStores();
+  let photo;
 
-    let photo;
+  function handleChangeImagen(e) {
+    e.preventDefault();
+    photo = e.target.files[0];
+    inputsSalientes.foto = photo;
+  }
 
-    function handleChangeImagen(e) {
-        e.preventDefault();
-        photo = e.target.files[0];
-        inputsSalientes.foto = photo;
-    }
-
-    const agregarColmena = (e) => {
-        store.updateAgregarColmenaForm(inputsSalientes);
-        store.setMedia({ photo });
-        const formData = new FormData();
-        formData.append("photo", photo);
-        const config = {
-            headers: { "content-type": "multipart/form-data" },
-        };
-        axios.post(`http://${process.env.REACT_APP_IP}:2222/api/colmena/photo`, formData, config)
-            .then(() => props.history.push("/home"))
-            .catch((error) => { throw error; })
-
+  const agregarColmena = (e) => {
+    store.updateAgregarColmenaForm(inputsSalientes);
+    store.setMedia({ photo });
+    const formData = new FormData();
+    formData.append("photo", photo);
+    const config = {
+      headers: { "content-type": "multipart/form-data" },
     };
+    axios
+      .post(
+        `http://${process.env.REACT_APP_IP}:2222/api/colmena/photo`,
+        formData,
+        config
+      )
+      .then(() => props.history.push("/home"))
+      .catch((error) => {
+        throw error;
+      });
+  };
 
-    const { inputsSalientes, handleInputChange, handleSubmit } = useForm(agregarColmena, {
-        direccionColmena: "",
-        especieAbejas: "",
-        foto: "",
-        nombreColmena: "",
-        tipoColmena: "",
-    });
+  const { inputsSalientes, handleInputChange, handleSubmit } = useForm(
+    agregarColmena,
+    {
+      direccionColmena: "",
+      especieAbejas: "",
+      foto: "",
+      nombreColmena: "",
+      tipoColmena: "",
+    }
+  );
 
-    return (<div>
-        <NavBar />
-        <YellowBanner>
-            <WrapperDivForImages>
-                <LogoYellowBanner src={require("../utils/lapizCirculo@2x.png")} />
-            </WrapperDivForImages>
-            <SpanBanner>Editar colmena</SpanBanner>
-        </YellowBanner>
-        <StyledForm onSubmit={(e) => { e.persist(); const evento = e; handleSubmit(evento); }}>
-            <FormContent>
-                <TextoAmarillo>Nombre Colmena</TextoAmarillo>
-                <StyledInput
-                    placeholder="Nombre de su colmeena"
-                    name="nombreColmena"
-                    value={inputsSalientes.nombreColmena}
-                    onChange={handleInputChange} />
-                <NotaInput>Esta será la etiqueta primaria para esta colmena</NotaInput>
-            </FormContent>
-            <FormContent>
-                <TextoAmarillo>Tipo de Colmena</TextoAmarillo>
-                <StyledSelect
-                    name="tipoColmena"
-                    value={inputsSalientes.tipoColmena}
-                    onChange={handleInputChange}>Tipo de Colmena
-                    <option selected value="langstroth">Langstroth</option>
-                    <option value="warre">Warre</option>
-                    <option value="topBar">Top Bar</option>
-                    <option value="otroTipo">Otro</option>
-                </StyledSelect>
-                <NotaInput>Seleccione el tipo de colmena que está utilizando</NotaInput>
-            </FormContent>
-            <FormContent>
-                <TextoAmarillo>Especie de Abejas</TextoAmarillo>
-                <StyledSelect
-                    name="especieAbejas"
-                    value={inputsSalientes.especieAbejas}
-                    defaultValue="Apis mellifera"
-                    onChange={handleInputChange}>Especie de Abejas
-                    <option selected value="apisMellifera">Apis mellifera</option>
-                    <option value="apisMelliferaCaucasia">Apis mellifera caucasia</option>
-                    <option value="apisMelliferaCarnica">Apis mellifera carnica</option>
-                    <option value="apisMelliferaLigustica">Apis mellifera ligustica</option>
-                    <option value="apisMelliferaMellifera">Apis mellifera mellifera</option>
-                    <option value="apisMelliferaScutellata">Apis mellifera scutellata</option>
-                    <option value="otraEspecie">Otra</option>
-                </StyledSelect>
-                <NotaInput>Seleccione una o multiples especies para híbridos</NotaInput>
-            </FormContent>
-            <FormContent>
-                <TextoAmarillo>Foto de Colmena</TextoAmarillo>
-                <ImageContainer>
-                    <input
-                        type="file"
-                        accept="image/*"
-                        id="file-input"
-                        onChange={handleChangeImagen} />
-
-                </ImageContainer>
-                <NotaInput>Tome una foto de su colmena</NotaInput>
-            </FormContent>
-            <FormContent>
-                <TextoAmarillo>Dirección de Colmena</TextoAmarillo>
-                <StyledInput
-                    name="direccionColmena"
-                    value={inputsSalientes.direccionColmena}
-                    onChange={handleInputChange} />
-                <NotaInput>Seleccione la localización de su colmena</NotaInput>
-            </FormContent>
-            <div style={{ display: "flex", justifyContent: "space-between" }}>
-                <FormAtrasButton onClick={(e) => {
-                    e.preventDefault();
-                    props.history.push("/agregarColmena");
-                }} />
-                <FormSubmitButton />
+  return (
+    <div>
+      <NavBar />
+      <YellowBanner>
+        <WrapperDivForImages>
+          <LogoYellowBanner src={require("../utils/lapizCirculo@2x.png")} />
+        </WrapperDivForImages>
+        <SpanBanner>Editar colmena</SpanBanner>
+      </YellowBanner>
+      <StyledForm
+        onSubmit={(e) => {
+          e.persist();
+          const evento = e;
+          handleSubmit(evento);
+        }}
+      >
+        <FormContent>
+          <TextoAmarillo>Nombre Colmena</TextoAmarillo>
+          <StyledInput
+            placeholder="Nombre de su colmena"
+            name="nombreColmena"
+            value={inputsSalientes.nombreColmena}
+            onChange={handleInputChange}
+          />
+          <NotaInput>
+            Esta será la etiqueta primaria para esta colmena
+          </NotaInput>
+        </FormContent>
+        <FormContent>
+          <TextoAmarillo>Tipo de Colmena</TextoAmarillo>
+          <StyledSelect
+            name="tipoColmena"
+            value={inputsSalientes.tipoColmena}
+            onChange={handleInputChange}
+          >
+            Tipo de Colmena
+            <option selected value="langstroth">
+              Langstroth
+            </option>
+            <option value="warre">Warre</option>
+            <option value="topBar">Top Bar</option>
+            <option value="otroTipo">Otro</option>
+          </StyledSelect>
+          <NotaInput>
+            Seleccione el tipo de colmena que está utilizando
+          </NotaInput>
+        </FormContent>
+        <FormContent>
+          <TextoAmarillo>Especie de Abejas</TextoAmarillo>
+          <StyledSelect
+            name="especieAbejas"
+            value={inputsSalientes.especieAbejas}
+            defaultValue="Apis mellifera"
+            onChange={handleInputChange}
+          >
+            Especie de Abejas
+            <option selected value="apisMellifera">
+              Apis mellifera
+            </option>
+            <option value="apisMelliferaCaucasia">
+              Apis mellifera caucasia
+            </option>
+            <option value="apisMelliferaCarnica">Apis mellifera carnica</option>
+            <option value="apisMelliferaLigustica">
+              Apis mellifera ligustica
+            </option>
+            <option value="apisMelliferaMellifera">
+              Apis mellifera mellifera
+            </option>
+            <option value="apisMelliferaScutellata">
+              Apis mellifera scutellata
+            </option>
+            <option value="otraEspecie">Otra</option>
+          </StyledSelect>
+          <NotaInput>
+            Seleccione una o multiples especies para híbridos
+          </NotaInput>
+        </FormContent>
+        <FormContent>
+          <TextoAmarillo>Foto de Colmena</TextoAmarillo>
+          <ImageContainer>
+            <div
+              style={{
+                display: "flex",
+                alignSelf: "stretch",
+                height: "4vh",
+                width: "50vw",
+                outline: "none",
+                borderRadius: "2px",
+                border: "1px solid",
+                borderColor: "#e5e3e3",
+                backgroundColor: "#ffff",
+                paddingTop: "0.2rem",
+              }}
+            >
+              <input
+                type="file"
+                accept="image/*"
+                id="file-input"
+                onChange={handleChangeImagen}
+              />
             </div>
-        </StyledForm>
-
-    </div >
-    );
+          </ImageContainer>
+          <NotaInput>Tome una foto de su colmena</NotaInput>
+        </FormContent>
+        <FormContent>
+          <TextoAmarillo>Dirección de Colmena</TextoAmarillo>
+          <StyledInput
+            name="direccionColmena"
+            placeholder="Dirección de colmena"
+            value={inputsSalientes.direccionColmena}
+            onChange={handleInputChange}
+          />
+          <NotaInput>Seleccione la localización de su colmena</NotaInput>
+        </FormContent>
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
+          <FormAtrasButton
+            onClick={(e) => {
+              e.preventDefault();
+              props.history.push("/agregarColmena");
+            }}
+          />
+          <FormSubmitButton />
+        </div>
+      </StyledForm>
+    </div>
+  );
 };
