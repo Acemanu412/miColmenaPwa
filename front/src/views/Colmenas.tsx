a55bf2d4e48c57712a7128d9fd3faee13b3bb153
Erika Stefania Castillo Barragan <erikaastef99@10.100.0.72>
2019-12-10 11:07:21 -0300

    ningun cambio

--- front/src/views/Colmenas.tsx
+++ front/src/views/Colmenas.tsx
@@ -12,8 +12,8 @@ import {
 } from "../styles/ColmenasStyles";
 
 let checkedColmenas = false;
-const IP = ${process.env.REACT_APP_IP} || "5.189.179.214"
-const PORT = ${process.env.REACT_APP_PORT} || "80"
+const IP = `${process.env.REACT_APP_IP}` || "5.189.179.214";
+const PORT = `${process.env.REACT_APP_PORT}` || "80";
 export const Colmenas: React.FC = (props) => {
   const store = useStores();
   const [colmenas, setColmenas] = useState([]);
@@ -31,15 +31,23 @@ export const Colmenas: React.FC = (props) => {
 
   return (
     <ColmenasContainer>
-
       {colmenas.map((colmena, index) => {
-        return (<Link to="/vistaColmena" key={index} style={{ textDecoration: "none", color: "black" }}>
-          <DataColmenaContainer>
-            <img src={`http://${IP}:${PORT}/fotos/${colmena.foto}`}
-              alt="" style={{ height: "70px" }} />
-            {colmena.nombre}
-          </DataColmenaContainer>
-        </Link>);
+        return (
+          <Link
+            to="/vistaColmena"
+            key={index}
+            style={{ textDecoration: "none", color: "black" }}
+          >
+            <DataColmenaContainer>
+              <img
+                src={`http://${IP}:${PORT}/fotos/${colmena.foto}`}
+                alt=""
+                style={{ height: "70px" }}
+              />
+              {colmena.nombre}
+            </DataColmenaContainer>
+          </Link>
+        );
       })}
 
       <ButtonColmenasContainer>
