5c4772dfccc779233fb3b6537f4e3ff09f761bd5
Acemanu412 <manuel.n.aceituno@gmail.com>
2019-12-10 15:53:36 -0300

    Paquete de prueba II: Plesk

--- front/build/service-worker.js
+++ front/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d35831f2ae497195279f1ca1797fa8ff.js"
+  "/precache-manifest.53574cf4a2841f6896bbecaf98352389.js"
 );
 
 self.addEventListener('message', (event) => {
