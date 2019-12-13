a0610943cb048db00803ce0c86a4ff67ff56036b
Acemanu412 <manuel.n.aceituno@gmail.com>
2019-12-13 10:40:29 -0300

    Update Colmena.js

--- back/models/Colmena.js
+++ back/models/Colmena.js
@@ -1,6 +1,9 @@
 const S = require("sequelize");
 const db = require("../config/db");
 
+// Desde el formulario AgregarColmenaEstandar se recibe mas informacion de la que se guarda 
+// (tipo de colmena, especie, localizacion) 
+
 class Colmena extends S.Model { }
 
 Colmena.init(
