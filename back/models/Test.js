cc7ce33a280a3febd96aeb401b898f753d630248
Acemanu412 <manuel.n.aceituno@gmail.com>
2019-12-13 10:51:22 -0300

    Update Test.js

--- back/models/Test.js
+++ back/models/Test.js
@@ -3,6 +3,9 @@ const db = require("../config/db");
 
 class Test extends S.Model { }
 
+// Este modelo fué hecho para testear la información del Arduino. Una vez que el Arduino envía información, se guardará el JSON
+// aquí como un string para poder analizarlo con mas tranquilidad.
+
 Test.init(
     {
         dataArduino: {
