9a675a4aa3526d44db94a35ff0a6ada86e4245cf
Acemanu412 <manuel.n.aceituno@gmail.com>
2019-12-13 11:33:49 -0300

    Update server.js

--- back/server.js
+++ back/server.js
@@ -9,6 +9,9 @@ const path = require("path");
 const db = require("./config/db");
 const routes = require("./routes");
 
+// Este middleware nos garantiza que no haya errores de CORS. Si se reciben errores de CORS en Firefox, recomendamos rehacer
+// la prueba en Chrome, ya que Firefox envía errores de CORS cuando tiene algún conflicto el certificado SSL.
+
 app.use(function (req, res, next) {
   let allowedOrigins = [req.headers.origin, req.headers.host];
   let origin = req.headers.origin || req.headers.host;
@@ -21,8 +24,11 @@ app.use(function (req, res, next) {
   next();
 });
 
+// Una carpeta estática se utiliza para levantar el paquete del front y la otra se utiliza para los audios e imágenes.
+
 app.use(express.static(path.resolve("../front/build")));
 app.use(express.static(path.join(__dirname, "/uploads/")));
+
 app.use(morgan("dev"));
 app.use(cookieParser());
 app.use(bodyParser.json());
@@ -36,6 +42,8 @@ app.use(
 );
 app.use(passport.initialize());
 app.use(passport.session());
+
+// Alterar el orden siguiente causará que no sean posibles pedidos a la rutas del back.
 
 app.use("/api", routes);
 
