7f6af066afd525f604e305d8f1a1dddff20fe685
Acemanu412 <manuel.n.aceituno@gmail.com>
2019-12-13 10:37:45 -0300

    Update Audio.js

--- back/models/Audio.js
+++ back/models/Audio.js
@@ -3,6 +3,8 @@ const db = require("../config/db");
 
 class Audio extends S.Model { }
 
+// Este modelo esta creado pero en la implementación actual no está siendo utilizado.
+
 Audio.init(
     {
         timestamp: {
